91eaaf12341d23caa20e1c221704e20bc0e88710
Jiramet2005 <jiramet.be@ku.th>
2026-09-13 15:53:56 +0700

    [dev] edit database

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -105,15 +105,14 @@ var todoGroup = app.MapGroup("/api/todos").WithTags("Todos");
 
 todoGroup.MapGet("/", async (AppDbContext db) =>
 {
-    var todos = await db.TodoItems.ToListAsync();
+    var todos = await db.Todos.ToListAsync();
     return todos.Count == 0 ? Results.NotFound() : Results.Ok(todos);
 });
 
 todoGroup.MapPost("/", async (AppDbContext db, TodoPostDto dto) =>
 {
-    var lastTodo = await db.TodoItems.OrderByDescending(t => t.Id).FirstOrDefaultAsync();
+    var lastTodo = await db.Todos.OrderByDescending(t => t.Id).FirstOrDefaultAsync();
     var nextId = lastTodo is null ? 1 : lastTodo.Id + 1;
-
     var todo = new TodoItem
     {
         Id = nextId,
@@ -122,7 +121,7 @@ todoGroup.MapPost("/", async (AppDbContext db, TodoPostDto dto) =>
         CreatedAt = DateTime.UtcNow
     };
 
-    db.TodoItems.Add(todo);
+    db.Todos.Add(todo);
     await db.SaveChangesAsync();
 
     var todoGetDto = new TodoGetDto(todo.Id, todo.Title, todo.IsCOmpleted);
