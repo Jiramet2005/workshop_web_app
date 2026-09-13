129e639b0e450dabd1888a84c6e5229cff94c9ef
Jiramet2005 <jiramet.be@ku.th>
2026-09-13 13:15:32 +0700

    Merge pull request #5 from Jiramet2005/feat/post-todo-endpoint
    (dev) add todo Post

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApi.Models;
+
+public class TodoItem
+{
+    public int Id { get; set; }
+    public stirng  Title { get; set; }
+    public bool IsCOmpleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
